c7cfd791079f60f21896166f361964508c23c94d
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-09 16:42:52 -0300

    feat: import do componente criado.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/App.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/App.jsx
@@ -1,4 +1,3 @@
-import FetchGet from './utils/Axios.jsx'
 import SearchBar from './components/SearchBar/SearchBar.jsx'
 function App() {
 
@@ -6,7 +5,6 @@ function App() {
   return (
     <>
       <SearchBar />
-      <FetchGet />
     </>
   )
 }
