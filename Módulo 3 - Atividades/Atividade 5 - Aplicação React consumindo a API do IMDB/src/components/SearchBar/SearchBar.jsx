59c6885195d2350a3286265cd28fa5030b2fe27a
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-09 16:40:17 -0300

    feat: implementação do componente searchBar.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/SearchBar/SearchBar.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/SearchBar/SearchBar.jsx
@@ -1,27 +1,46 @@
-import './SearchBar.css'
+import { useState } from 'react';
+import './SearchBar.css';
+import FetchGet from '../../utils/Axios';
 
 function SearchBar() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [submittedQuery, setSubmittedQuery] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setSubmittedQuery(searchQuery);
+  };
 
   return (
-    <>
-      <div className='search-container'>
-          <div className='input-group'>
-            <div className='input-icon'>
-              <button className='btn-submit' type='submit'>
-                <img className="img-search" src="../../../public/img/lupa.png" alt="Icone de uma lupa" />
-              </button>
-            </div>
-
-            <input
-            className="search-input"
-            type="text" name="search"
-            id="search"
-            placeholder="Pesquise um filme" 
+    <div className='search-container'>
+      <form className='input-group' onSubmit={handleSubmit}>
+        <div className='input-icon'>
+          <button
+            className='btn-submit'
+            type="submit"
+          >
+            <img
+              className="img-search"
+              src="/img/lupa.png"
+              alt="Icone de uma lupa"
             />
-          </div>
-      </div>
-    </>
-  )
+          </button>
+        </div>
+
+        <input
+          className="search-input"
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Pesquise um filme"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+      </form>
+
+      {<FetchGet searchInput={submittedQuery} />}
+    </div>
+  );
 }
 
-export default SearchBar
+export default SearchBar;
