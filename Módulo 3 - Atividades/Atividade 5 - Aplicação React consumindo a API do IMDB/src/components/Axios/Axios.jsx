34c405e594cd0841c5c549a607b9364d0889ac4d
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-10 18:41:03 -0300

    feat: importado e implementado uso do componente PageNavigation através do axios. Modificações de requisição para receber número de páginas.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Axios/Axios.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Axios/Axios.jsx
@@ -1,8 +1,11 @@
 import { useEffect, useState } from 'react';
 import Card from '../Card/Card';
+import PageNavigation from '../PageNavigation/PageNavigation';
 
 const FetchGet = ({ searchInput }) => {
     const [movies, setMovies] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
@@ -12,12 +15,13 @@ const FetchGet = ({ searchInput }) => {
         const getMovies = async () => {
             setLoading(true);
             try {
-                const response = await fetch(`http://www.omdbapi.com/?apikey=31f23909&s=${searchInput}`);
+                const response = await fetch(`http://www.omdbapi.com/?apikey=31f23909&s=${searchInput}&page=${currentPage}`);
                 if (!response.ok) throw new Error('Falha na requisição.');
 
                 const data = await response.json();
                 if (data.Response === "True") {
                     setMovies(data.Search);
+                    setTotalPages(data.totalResults);
                 } else {
                     setMovies([]);
                 }
@@ -29,13 +33,21 @@ const FetchGet = ({ searchInput }) => {
         };
 
         getMovies();
-    }, [searchInput]);
+    }, [searchInput, currentPage]);
 
     return (
         <div>
             {loading && <p>Carregando...</p>}
             {error && <p style={{ color: 'red' }}>Erro: {error}</p>}
-            <Card movies={movies} searchInput={searchInput} />
+            <Card
+                movies={movies}
+                searchInput={searchInput}
+            />
+            <PageNavigation
+                currentPage={currentPage}
+                totalPages={totalPages}
+                setCurrentPage={setCurrentPage}
+            />
         </div>
     );
 };
