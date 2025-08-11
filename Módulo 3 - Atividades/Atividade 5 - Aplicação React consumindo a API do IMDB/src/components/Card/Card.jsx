6e4c26464be71c0e079ebe0585fd1a6541e380b1
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-11 11:17:05 -0300

    feat: incrementado função onShowDetails recebida por props para id do filme selecionado.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Card/Card.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Card/Card.jsx
@@ -1,17 +1,22 @@
 import './Card.css';
 
-const Card = ({ movies, searchInput }) => {
+const Card = ({ movies, searchInput, onShowDetails }) => {
 
     return (
         <div>
             <ul className='card-container'>
                 {movies.length > 0 || !searchInput? (
                     movies.map((movie) => (
-                        <li key={movie.imdbID} className='card-movie'>
+                        <li key={movie.imdbID} className='card-movie' onClick={() => onShowDetails(movie.imdbID)}>
                             <img src={movie.Poster} alt={movie.Title} />
                             <p className='title'>{movie.Title}</p>
                             <p className='ano'>{movie.Year}</p>
-                            <button className='btn-details'>Ver detalhes</button>
+                            <button
+                                className='btn-details'
+                                onClick={() => onShowDetails(movie.imdbID)}
+                            >
+                                Ver detalhes
+                            </button>
                         </li>
                     ))
                 ) : (
