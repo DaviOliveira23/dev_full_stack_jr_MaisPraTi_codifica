7020dc811300d2857140233160ed78e8ec874ffe
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-11 15:03:11 -0300

    feat: implementado renderização do botão/componente isFavorite no dentro do card de cada filme. Componente card fica responsável por guardar favoritos no storage.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Card/Card.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/components/Card/Card.jsx
@@ -1,13 +1,29 @@
 import './Card.css';
+import { useState, useEffect } from 'react';
+import IsFavorite from '../isFavorite/isFavorite';
 
 const Card = ({ movies, searchInput, onShowDetails }) => {
+    const [favoritos, setFavoritos] = useState(() => {
+        const stored = localStorage.getItem('favoritos');
+        return stored ? JSON.parse(stored) : [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem('favoritos', JSON.stringify(favoritos));
+    }, [favoritos]);
 
     return (
         <div>
             <ul className='card-container'>
                 {movies.length > 0 || !searchInput? (
                     movies.map((movie) => (
-                        <li key={movie.imdbID} className='card-movie' onClick={() => onShowDetails(movie.imdbID)}>
+                        <li key={movie.imdbID} className='card-movie'>
+                            <IsFavorite
+                                key={movie.imdbID}
+                                movie={movie}
+                                favoritos={favoritos}
+                                setFavoritos={setFavoritos}
+                            />
                             <img src={movie.Poster} alt={movie.Title} />
                             <p className='title'>{movie.Title}</p>
                             <p className='ano'>{movie.Year}</p>
