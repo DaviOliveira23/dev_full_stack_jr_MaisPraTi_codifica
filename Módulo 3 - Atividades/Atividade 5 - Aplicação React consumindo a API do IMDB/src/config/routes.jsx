1806ae650fc5b1e78aba9495b24a36849e436ed8
DaviOliveira23 <davimesquitade@gmail.com>
2025-08-20 16:30:06 -0300

    feat: criado rota para novo componente favorites.

--- Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/config/routes.jsx
+++ Módulo 3 - Atividades/Atividade 5 - Aplicação React consumindo a API do IMDB/src/config/routes.jsx
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/Home/Home";
+import Favorites from "../components/Favorites/Favorites";
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <>
             <Route path="/" element={ <RootLayout /> } >
                 <Route path="/" element={ <Home /> } />
+                <Route path="/favoritos" element={ <Favorites /> } />
             </Route>
         </>
     )
